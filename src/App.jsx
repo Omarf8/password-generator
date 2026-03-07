4541dfd306d6267bd8727a163a1f2ff4d4ec6160
Omarf8 <omarfigueroa223@gmail.com>
2026-03-06 22:55:24 -0600

    Complete HTML skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [len, setLength] = useState(12)
+  const [upper, setUpper] = useState(true);
+  const [lower, setLower] = useState(true);
+  const [nums, setNums] = useState(true);
+  const [sym, setSym] = useState(true);
 
   return (
     <>
       <div id="container">
         <h3>Password Generator</h3>
         <div className="setting">
-          <label htmlFor="length">Password Length: </label>
+          <label htmlFor="length">Password Length</label>
           <input id="length" type="number" min="5" max="20" value={len} onChange={e => setLength(e.target.value)} />
         </div>
         <div className="setting">
-
+          <label htmlFor="uppercase">Include Uppercase Letters</label>
+          <input id="uppercase" type="checkbox" checked={upper} onChange={e => setUpper(e.target.checked)}/>
         </div>
         <div className="setting">
-
+          <label htmlFor="lowercase">Include Lowercase Letters</label>
+          <input id="lowercase" type="checkbox" checked={lower} onChange={e => setLower(e.target.checked)}/>
         </div>
         <div className="setting">
-
+          <label htmlFor="numbers">Include Numbers</label>
+          <input id="numbers" type="checkbox" checked={nums} onChange={e => setNums(e.target.checked)}/>
         </div>
         <div className="setting">
-
+          <label htmlFor="symbols">Include Special Symbols</label>
+          <input id="symbols" type="checkbox" checked={sym} onChange={e => setSym(e.target.checked)}/>
         </div>
         <button>Generate</button>
       </div>
