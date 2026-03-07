62a927224471dd2178db84980d7ed834061b68f2
Omarf8 <omarfigueroa223@gmail.com>
2026-03-06 22:27:11 -0600

    Add HTML skeleton and length setting

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,30 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [len, setLength] = useState(12)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="container">
+        <h3>Password Generator</h3>
+        <div className="setting">
+          <label htmlFor="length">Password Length: </label>
+          <input id="length" type="number" min="5" max="20" value={len} onChange={e => setLength(e.target.value)} />
+        </div>
+        <div className="setting">
+
+        </div>
+        <div className="setting">
+
+        </div>
+        <div className="setting">
+
+        </div>
+        <div className="setting">
+
+        </div>
+        <button>Generate</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
