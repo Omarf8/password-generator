4c36d5d05f88dbd78f06a7c966fa4fe8120b3aa0
Omarf8 <omarfigueroa223@gmail.com>
2026-03-08 11:14:17 -0500

    Update HTML skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -12,34 +12,36 @@ function App() {
   return (
     <>
       <div id="container">
-        <h3>Password Generator</h3>
-        <div className="password-res">
-          <span></span>
-          <button>
+        <h2>Password Generator</h2>
+        <div id="password-res">
+          <span className="result"></span>
+          <button className="clip">
             <FontAwesomeIcon icon={faClipboard} />
           </button>
         </div>
-        <div className="setting">
-          <label htmlFor="length">Password Length</label>
-          <input id="length" type="number" min="5" max="20" value={len} onChange={e => setLength(e.target.value)} />
+        <div id="settings">
+          <div className="setting">
+            <label htmlFor="length">Password Length</label>
+            <input id="length" type="number" min="5" max="20" value={len} onChange={e => setLength(e.target.value)} />
+          </div>
+          <div className="setting">
+            <label htmlFor="uppercase">Include Uppercase Letters</label>
+            <input id="uppercase" type="checkbox" checked={upper} onChange={e => setUpper(e.target.checked)}/>
+          </div>
+          <div className="setting">
+            <label htmlFor="lowercase">Include Lowercase Letters</label>
+            <input id="lowercase" type="checkbox" checked={lower} onChange={e => setLower(e.target.checked)}/>
+          </div>
+          <div className="setting">
+            <label htmlFor="numbers">Include Numbers</label>
+            <input id="numbers" type="checkbox" checked={nums} onChange={e => setNums(e.target.checked)}/>
+          </div>
+          <div className="setting">
+            <label htmlFor="symbols">Include Special Symbols</label>
+            <input id="symbols" type="checkbox" checked={sym} onChange={e => setSym(e.target.checked)}/>
+          </div>
         </div>
-        <div className="setting">
-          <label htmlFor="uppercase">Include Uppercase Letters</label>
-          <input id="uppercase" type="checkbox" checked={upper} onChange={e => setUpper(e.target.checked)}/>
-        </div>
-        <div className="setting">
-          <label htmlFor="lowercase">Include Lowercase Letters</label>
-          <input id="lowercase" type="checkbox" checked={lower} onChange={e => setLower(e.target.checked)}/>
-        </div>
-        <div className="setting">
-          <label htmlFor="numbers">Include Numbers</label>
-          <input id="numbers" type="checkbox" checked={nums} onChange={e => setNums(e.target.checked)}/>
-        </div>
-        <div className="setting">
-          <label htmlFor="symbols">Include Special Symbols</label>
-          <input id="symbols" type="checkbox" checked={sym} onChange={e => setSym(e.target.checked)}/>
-        </div>
-        <button>Generate</button>
+        <button id="generate">Generate</button>
       </div>
     </>
   )
